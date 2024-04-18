874ec300f9050d115597263a9eb85f4f99b6587f
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2024-04-18 13:31:59 -0300

    :books: doc: adicionando documentação das funções de grafo.h

--- grafo/grafo.h
+++ grafo/grafo.h
@@ -39,34 +39,35 @@ void grau_arquivo(int *grau, int tamanho);
 int *vertices_isolados(int **matriz, int tamanho, int *num_vertices);
 
 /* Função vertices_multiplos_5
- - retorna um vetor com os vértices múltiplos de 5.
+ - Retorna um vetor com os vértices múltiplos de 5.
 */
 int *vertices_multiplos_5(int **matriz, int tamanho, int *num_vertices);
 
 /* Função subgrafo_induzido
- - gera um subgrafo induzido, a partir dos vértices inseridos.
+ - Gera um subgrafo induzido, a partir dos vértices inseridos.
 */
 int **subgrafo_induzido(int **matriz, int *vertices, int tamanho, int tamanho_induzido);
 
 /* Função salva_grafo
- - escreve a matriz booleana do grafo em um arquivo.
+ - Escreve a matriz booleana do grafo em um arquivo.
 */
 void salva_grafo(int **matriz_subgrafo, int tamanho, char *nome_arquivo);
 
 /* Função verifica_clique
+ - Testa se a matriz do subgrafo inserido é um clique.
 */
 int verifica_clique(int **matriz, int tamanho);
 
 /* Função copia_vetor
+ - Faz a cópia de um vetor, elemento por elemento.
 */
 int *copia_vetor(int *vetor_dst, int *vetor_src, int tam_vetor_dst, int tam_vetor_src);
 
 /* Função candidator_clique
+ - Testa todas as possibilidades de cominação entre vértices conectados
+   ao vértice de uma linha, até achar o maior clique.
 */
 int *candidatos_clique(int **matriz, int tamanho, int vertice_linha, int vertice_iter, int *tamanho_clique);
-// int *candidatos_clique(int **matriz, int tamanho, int vertice_linha, int *tamanho_clique);
-
-// int *procura_maior_clique(int **matriz, int *vetor_candidatos, int tamanho, int vertice_linha, int vertice_iter, int *tamanho_clique);
 
 /* Função conexao_primeiro_ultimo
  - Verifica se há um caminho que conecte os vertices
